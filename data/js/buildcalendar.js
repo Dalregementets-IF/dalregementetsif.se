29e9fa6236b423e9c7e5f27206265e0f65b493fa
Tobias Dély <cleverhatcamouflage@gmail.com>
2024-02-19 20:39:25 +0100

    style(js): `"`->`'`, arrow function expressions when possible

--- data/js/buildcalendar.js
+++ data/js/buildcalendar.js
@@ -21,7 +21,7 @@ function buildCalendar(dlpath) {
 			customButtons: {
 				download: {
 					text: 'Ladda ner kalender',
-					click: function () {window.location = dlpath}
+					click: () => {window.location = dlpath}
 				}
 			},
 			initialView: 'dayGridMonth',
@@ -47,17 +47,17 @@ function buildCalendar(dlpath) {
 			},
 		})
 		cal.render()
-		$.get(dlpath).then(function (data) {
+		$.get(dlpath).then((data) => {
 			var c = new ICAL.Component(ICAL.parse(data.trim()))
-			var events = $.map(c.getAllSubcomponents("vevent"), function(item){
-				if(item.getFirstPropertyValue("class") == "PRIVATE"){
+			var events = $.map(c.getAllSubcomponents('vevent'), (item) => {
+				if(item.getFirstPropertyValue('class') == 'PRIVATE'){
 					return null
 				} else {
 					var res = {
-						"title": item.getFirstPropertyValue("summary"),
-						"location": item.getFirstPropertyValue("location"),
+						'title': item.getFirstPropertyValue('summary'),
+						'location': item.getFirstPropertyValue('location'),
 					};
-					var rrule = item.getFirstPropertyValue("rrule")
+					var rrule = item.getFirstPropertyValue('rrule')
 					if (rrule != null) {
 						//event recurs
 						res.rrule = {}
@@ -66,17 +66,17 @@ function buildCalendar(dlpath) {
 						if (rrule.until) res.rrule.until = rrule.until.toString()
 						if (rrule.until) res.rrule.until = rrule.until.toString()
 						if (rrule.interval) res.rrule.interval = rrule.interval
-						var dtstart = item.getFirstPropertyValue("dtstart").toString()
-						var dtend = item.getFirstPropertyValue("dtend").toString()
+						var dtstart = item.getFirstPropertyValue('dtstart').toString()
+						var dtend = item.getFirstPropertyValue('dtend').toString()
 						res.rrule.dtstart = dtstart
 						//count duration ms
 						var startdate = new Date(dtstart)
 						var enddate = new Date(dtend)
 						res.duration = enddate - startdate
 					} else {
-						if (item.hasProperty("dtstart") && item.hasProperty("dtend")) {
-							res.start = item.getFirstPropertyValue("dtstart").toString()
-							res.end = item.getFirstPropertyValue("dtend").toString()
+						if (item.hasProperty('dtstart') && item.hasProperty('dtend')) {
+							res.start = item.getFirstPropertyValue('dtstart').toString()
+							res.end = item.getFirstPropertyValue('dtend').toString()
 						} else {
 							return null
 						}
--- data/js/filebrowser.js
+++ data/js/filebrowser.js
@@ -4,14 +4,14 @@ This is an altered JS based on the Nginx Fancyindex module theme
 Nginxy by lfelipe1501: https://github.com/lfelipe1501/Nginxy
 */
 // Configure Nginxy here:
-var websiteName = "Dalregementets IF"
-var websiteURL = ""
-var parentDirText = "Föregående katalog"
+var websiteName = 'Dalregementets IF'
+var websiteURL = ''
+var parentDirText = 'Föregående katalog'
 // End of normal settings.
 
-$(document).ready(function() {
+$(document).ready(() => {
 	// Working on nginx HTML and applying settings.
-	var text = $("h1").text()
+	var text = $('h1').text()
 	var array = text.split('/')
 	var last = array[array.length-2]
 	var currentDir = last.charAt(0) + last.slice(1)
@@ -23,21 +23,21 @@ $(document).ready(function() {
 
 	// Updating page title.
 	document.title = currentDir + ' – ' + websiteName
-	$("h1").html(currentDir)
+	$('h1').html(currentDir)
 
 	// Establish supported formats.
 	var list = new Array()
-	var formats = ["bin", "jpg", "gif", "bmp", "png", "html", "css", "zip", "iso", "tiff", "ico", "psd", "pdf", "exe", "rar", "deb", "swf", "7z", "doc", "docx", "xls", "xlsx", "pptx", "ppt", "txt", "php", "js", "c", "cpp", "torrent", "sql", "wmv", "avi", "mp4", "mp3", "wma", "ogg", "msg", "wav", "py", "java", "gzip", "jpeg", "raw", "cmd", "bat", "sh", "svg"]
+	var formats = ['bin', 'jpg', 'gif', 'bmp', 'png', 'html', 'css', 'zip', 'iso', 'tiff', 'ico', 'psd', 'pdf', 'exe', 'rar', 'deb', 'swf', '7z', 'doc', 'docx', 'xls', 'xlsx', 'pptx', 'ppt', 'txt', 'php', 'js', 'c', 'cpp', 'torrent', 'sql', 'wmv', 'avi', 'mp4', 'mp3', 'wma', 'ogg', 'msg', 'wav', 'py', 'java', 'gzip', 'jpeg', 'raw', 'cmd', 'bat', 'sh', 'svg']
 
 	// Scan all files in the directory, check the extensions and show the right MIME-type image.
 	$('td a').each(function() {
 		let fileExt = ""
-		let parts = $(this).attr('href').split(".")
+		let parts = $(this).attr('href').split('.')
 		if (parts.length > 1) {
 			fileExt = parts[parts.length - 1]
 		}
 
-		if ($(this).text().indexOf("Parent directory") >= 0) {
+		if ($(this).text().indexOf('Parent directory') >= 0) {
 			// Add an icon for the go-back link.
 			$(this).html('<img class="icons" src="/img/icons/home.webp">' + parentDirText)
 		} else if (fileExt.length > 0) {
